15ec86e34b108588608d4575244ca77dd980d244
Sophiasol24 <sophiasolstein@gmail.com>
2025-03-31 17:20:20 +0200

    Employee som kommentar indtil videre

--- src/main/java/dtu/example/backend/task.java
+++ src/main/java/dtu/example/backend/task.java
@@ -2,7 +2,7 @@ package dtu.example.backend;
 
 public class task {
     private String description; 
-    private Employee assignedEmployee; 
+    //private Employee assignedEmployee; 
     private int budgetedHours; 
     private int spentHours;
     private List<Task> ListAllTasks = new ArrayList<Task>();
