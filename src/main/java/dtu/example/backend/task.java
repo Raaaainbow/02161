e9d08d1624e7a7848ba7e0b33bbca64d7946f3cd
Sophiasol24 <sophiasolstein@gmail.com>
2025-03-31 17:17:34 +0200

    Start på task klassen

---
+++ src/main/java/dtu/example/backend/task.java
@@ -0,0 +1,10 @@
+package dtu.example.backend;
+
+public class task {
+    private String description; 
+    private Employee assignedEmployee; 
+    private int budgetedHours; 
+    private int spentHours;
+    private List<Task> ListAllTasks = new ArrayList<Task>();
+    public changebudgettedHours(int NewBudget) 
+}
