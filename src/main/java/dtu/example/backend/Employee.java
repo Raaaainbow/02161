21c8fe2e76ebc9b59ad1ba4781039f08f9d154d2
KatarinaHF <katarina@the-funch.dk>
2025-04-01 18:13:33 +0200

    Employee og ProjectLeader

---
+++ src/main/java/dtu/example/backend/Employee.java
@@ -0,0 +1,25 @@
+package dtu.example.backend;
+
+import java.util.ArrayList;
+
+public class Employee {
+    private ArrayList<Project> project;
+    private String name;
+    private String ID;
+
+    public Employee(String name, String ID){
+
+    }
+
+    public String createTask(){
+        return task;
+    }
+
+    public void assignTask(){
+
+    }
+
+    public void assignProjectLeader(Employee employee){
+
+    }
+}
