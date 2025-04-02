7212f714adfd68e791d1e5b4a1bf2766372ccfe3
CarolinePra <caroline.p.pradel@gmail.com>
2025-04-02 13:22:54 +0200

    Create TimeRegistration.java
    TimeRegistration created, variables initialized

---
+++ src/main/java/dtu/example/backend/TimeRegistration.java
@@ -0,0 +1,23 @@
+package dtu.example.backend;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class TimeRegistration {
+    private LocalDateTime ShiftStart;
+    private LocalDateTime ShiftEnd;
+    private LocalDate Date;
+
+    public void changeTimeReg(LocalDateTime shift, LocalDateTime updated){
+
+    }
+
+    public void deleteTimeReg(LocalDate shift){
+
+    }
+
+    public void listTimeReg(){
+        
+    }
+    
+}
