6ba5809fc0ed635eab02ea6ac0ffe0fec2144e60
Sophiasol24 <sophiasolstein@gmail.com>
2025-04-02 13:40:36 +0200

    Arraylist

--- src/main/java/dtu/example/backend/task.java
+++ src/main/java/dtu/example/backend/task.java
@@ -2,9 +2,11 @@ package dtu.example.backend;
 
 public class task {
     private String description; 
-    //private Employee assignedEmployee; 
+    private Employee assignedEmployee; 
     private int budgetedHours; 
     private int spentHours;
-    private List<Task> ListAllTasks = new ArrayList<Task>();
-    public changebudgettedHours(int NewBudget) 
+    private Arraylist<Task> ListAllTasks; 
+    public void changebudgettedHours(int NewBudget) {
+        
+    }
 }
