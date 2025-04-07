9fd893208389954d647f4c4b6ce59c7dbaeb2e0b
CarolinePra <caroline.p.pradel@gmail.com>
2025-04-07 14:51:36 +0200

    Methods

--- src/main/java/dtu/example/backend/TimeRegistration.java
+++ src/main/java/dtu/example/backend/TimeRegistration.java
@@ -7,17 +7,44 @@ public class TimeRegistration {
     private LocalDateTime ShiftStart;
     private LocalDateTime ShiftEnd;
     private LocalDate Date;
+    private boolean deleted;
 
-    public void changeTimeReg(LocalDateTime shift, LocalDateTime updated){
-
+    public TimeRegistration(LocalDateTime ShiftStart, LocalDateTime ShiftEnd, LocalDate Date){
+        this.ShiftStart = ShiftStart;
+        this.ShiftEnd = ShiftEnd;
+        this.Date = Date;
     }
 
-    public void deleteTimeReg(LocalDate shift){
-
+    public void deleteTimeReg(){
+        deleted = true;
     }
 
-    public void listTimeReg(){
-        
+    public boolean isDeleted() {
+        return deleted;
+    }
+
+    public void setShiftStart(LocalDateTime ShiftStart){
+        this.ShiftStart = ShiftStart;
+    }
+
+    public LocalDateTime getShiftStart(){
+        return ShiftStart;
+    }
+
+    public void setShiftEnd(LocalDateTime ShiftEnd){
+        this.ShiftEnd = ShiftEnd;
+    }
+
+    public LocalDateTime getShiftEnd(){
+        return ShiftEnd;
+    }
+
+    public void setLocalDate(LocalDate Date){
+        this.Date = Date;
+    }
+
+    public LocalDate getLocalDate(){
+        return Date;
     }
     
 }
