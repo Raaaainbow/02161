a6507e31e152bf53ca79faea07273b50d2b76cd2
Raaaainbow <sebastian@tjr.dk>
2025-04-23 07:32:58 +0200

    Project getters and setters created

--- src/main/java/dtu/example/Project.java
+++ src/main/java/dtu/example/Project.java
@@ -1,5 +1,55 @@
 package dtu.example;
 
 public class Project {
+    private String title;
+    private String Id;
+    private int startWeek;
+    private int endWeek;
+	private String projectLead;
 
+    // setters & getters
+
+    public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+    
+	public String getId() {
+		return Id;
+	}
+
+	public void setId(String id) {
+		Id = id;
+	}
+    
+	public int getStartWeek() {
+		return (startWeek > 0 && startWeek < 52)? startWeek: 0;
+	}
+    
+	public void setStartWeek(int startWeek) {
+        if (startWeek > 0 && startWeek < 52) {
+            this.startWeek = startWeek;   
+        }
+	}
+
+	public int getEndWeek() {
+		return (endWeek > 0 && endWeek < 52)? endWeek: 0;
+	}
+
+	public void setEndWeek(int endWeek) {
+        if (endWeek > 0 && endWeek < 52) {
+            this.endWeek = endWeek;
+        }
+	}
+
+	public String getProjectLead() {
+		return projectLead;
+	}
+    
+	public void setProjectLead(String projectLead) {
+		this.projectLead = projectLead;
+	}
 }
