5ea68f9925b13cb51af44523e27a2ba1425143c5
Sophiasol24 <sophiasolstein@gmail.com>
2025-05-09 14:52:16 +0200

    Employee class merge

--- src/main/java/dtu/example/Employee.java
+++ src/main/java/dtu/example/Employee.java
@@ -7,14 +7,20 @@ import java.util.List;
 
 public class Employee {
     private String initials;
-    private boolean employeeExists = false; 
-    private List<String> employeeList = new ArrayList<>();
+    private boolean employeeExists = false;
+    private static List<String> employeeList = new ArrayList<>();
     private String newInitials;
+    private static List<String> employeeListProject = new ArrayList<>();
+    private boolean employeeExistsinproject = false;
+    private static List<String> projectLeaderListProject = new ArrayList<>();
+    private boolean projectLeaderExistsinproject = false;
 
-    public Employee(String initials){
+    public Employee(String initials) {
         setInitials(initials);
         employeeList.add(initials);
         checkEmployeeExists(initials);
+        employeeListProject.add(initials);
+        checkEmployeeExistsinProject(initials);
     }
 
     public void checkEmployeeExists(String initials) {
@@ -22,7 +28,7 @@ public class Employee {
     }
 
     public Boolean employeeExists() {
-        return employeeExists; 
+        return employeeExists;
     }
 
     public String changeInitials(String newInitials) {
@@ -35,14 +41,26 @@ public class Employee {
     }
 
     public String getInitials() {
-        return initials; 
+        return initials;
     }
 
-    public void employeeExistsinproject(String initials) {
-        employeeExistsinproject(initials) = employeeList.contains(initials);
+    public void checkEmployeeExistsinProject(String initials) {
+        employeeExistsinproject = employeeListProject.contains(initials);
+    }
+
+    public boolean employeeExistsinproject(String initials) {
+        return employeeExistsinproject;
+    }
+
+    public void checkEmployeeisProjectLeader(String initials) {
+        projectLeaderExistsinproject = projectLeaderListProject.contains(initials);
+    }
+
+    public boolean projectLeaderExistsinproject(String initials) {
+        return employeeExistsinproject;
     }
 
     public boolean createTask(Project project, Task task) {
-    return project.addTaskAsEmployee(this, task);
+        return project.addTaskAsEmployee(this, task);
     }
 }
