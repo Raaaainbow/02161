7ddecd654b881eaa1bc81e067072d127596f7dfb
Raaaainbow <seba7204@gmail.com>
2025-05-11 10:22:45 +0200

    initial commit for project leader feature, tests do not pass

--- features/projectleader.feature
+++ features/projectleader.feature
@@ -1,9 +1,32 @@
-# Feature: Project leader 
-#     Description: Adding a task to the project if a project leader exists
-#     Acotr: Project leader 
+Feature: Project Leader management
+  As an administrator
+  I want to manage project leaders who are a type of employee
+  So that they can be assigned leadership roles in projects
 
-#     Scenario: A project leader wants to add a task to the project
-#         Given a project with a project leader exists 
-#         And the project leader is loggen in
-#         When the project leader adds a task to the project
-#         Then a task is created in the project 
+  Background:
+    Given the following employees exist: "EET" "HPH" "SES"
+    And the database is initialised
+
+  Scenario: Promote an existing employee to project leader
+    Given the project "P-255" exists
+    When I promote employee with initials "EET" to project leader on "P-255"
+    Then the employee with initials "EET" should be a project leader on "P-255"
+
+  Scenario: Create a new project leader
+    Given the project "P-255" exist
+    And the project "P-255" does not have a project leader
+    When "EET" assigns them self as project leader
+    Then a project leader with initials "EET" should exist
+    And they should be assigned to the "P-255" project
+
+    #  Scenario: View all project leaders
+    #    Given "EET" is project leader in "P-255" 
+    #    And "HPH" is project leader in "P-256"
+    #    When I view the list of project leaders
+    #    Then The list will contain "EET" with "P-255"
+    #    And The list will contain "HPH" with "P-256"
+    #
+    #  Scenario: Remove project leader role
+    #    Given employee with initials "SES" is a project leader
+    #    When I remove the project leader role from employee with initials "SES"
+    #    Then employee with initials "SES" should no longer be a project leader
--- src/test/java/hellocucumber/ProjectLeaderSteps.java
+++ src/test/java/hellocucumber/ProjectLeaderSteps.java
@@ -1,39 +1,68 @@
 package hellocucumber;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import dtu.example.Database;
 import dtu.example.Employee;
 import dtu.example.Project;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-import dtu.example.Employee;
-
 public class ProjectLeaderSteps {
-    Employee projectLeader;
-    Employee employee; 
-    Project project;
+    Database database = new Database();
 
+    @Given("the database is initialised")
+    public void theDatabaseIsInitialised() {
+        assertTrue(this.database != null);
+    }
 
-    // @Given("a project with a project leader exists")
-    // public void aProjectWithAProjectLeaderExists() {
-    //     project.getProjectNumber();
-    //     String initials = "abcd"; 
-    //     projectLeader = new Employee(initials);
-    //     project.setProjectLead(projectLeader.getInitials());
-    // }
+    @Given("the following employees exist: {string} {string} {string}")
+    public void theFollowingEmployeesExist(String string, String string2, String string3) {
+        database.createEmployee(string);
+        database.createEmployee(string2);
+        database.createEmployee(string3);
+    }
 
-    // @Given("the project leader is loggen in")
-    // public void theProjectLeaderIsLoggenIn() {
-        
-    // }
+    @Given("the project {string} exists")
+    public void theProjectExists(String string) {
+        database.createProject(string);;
+        assertTrue(database.projectExistsNumber(string));
+    }
 
-    // @When("the project leader adds a task to the project")
-    // public void theProjectLeaderAddsATaskToTheProject() {
-        
-    // }
+    @When("I promote employee with initials {string} to project leader on {string}")
+    public void iPromoteEmployeeWithInitialsToProjectLeaderOn(String initials, String projectNumber) {
+        Project project = database.getProjectByNumber(projectNumber);
+        project.setProjectLead(initials);
+    }
 
-    // @Then("a task is created in the project")
-    // public void aTaskIsCreatedInTheProject() {
-        
-    // }
+    @Then("the employee with initials {string} should be a project leader on {string}")
+    public void theEmployeeWithInitialsShouldBeAProjectLeaderOn(String initials, String projectNumber) {
+        Project project = database.getProjectByNumber(projectNumber);
+        assertEquals(initials, project.getProjectLead());
+    }
+
+    @Given("the project {string} exist")
+    public void theProjectExist(String projectNumber) {
+        assertTrue(database.getProjectByNumber(projectNumber) != null);
+    }
+
+    @Given("the project {string} does not have a project leader")
+    public void theProjectDoesNotHaveAProjectLeader(String projectNumber) {
+        Project project = database.getProjectByNumber(projectNumber);
+        assertTrue(!(project.projectLeaderInProject()));
+    }
+
+    @When("{string} assigns them self as project leader on project {string}")
+    public void assignsThemSelfAsProjectLeader(String initials, String projectNumber) {
+        Project project = database.getProjectByNumber(projectNumber);
+        project.setProjectLead(initials);
+    }
+
+    @Then("a project leader with initials {string} should exist on project {string}")
+    public void aProjectLeaderWithInitialsShouldExist(String initials, String projectNumber) {
+        Project project = database.getProjectByNumber(projectNumber);
+        assertEquals(initials, project.getProjectLead());
+    }
 }
